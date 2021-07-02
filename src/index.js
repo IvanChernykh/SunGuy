3c2459384e584cacb48d762c39f535cb24cc3ab6
Ivan Chernykh <ivanchernykh98@gmail.com>
2021-07-02 18:51:44 +0300

    finished spawnObstacle func

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './scss/style.scss'
 
-const menus = document.querySelectorAll('.screen__menu')
 const startMenu = document.querySelector('.screen__main')
 const controlMenu = document.querySelector('.screen__controls')
 const endGameMenu = document.querySelector('.screen__endgame')
+const menus = [startMenu, controlMenu, endGameMenu]
 controlMenu.classList.add('hide')
 endGameMenu.classList.add('hide')
 
@@ -39,9 +39,56 @@ function startGame() {
     screenGame.classList.add('screen__game')
     startMenu.insertAdjacentElement('beforebegin', screenGame)
     screenGame.innerHTML = getGameHTML
+    const char = screenGame.querySelector('.game__character')
+    let gameSpeed = 1
+    let obstCount = 0;
+
+    document.addEventListener('keydown', event => { if (event.code === 'Space') jump() })
+    screenGame.addEventListener('click', jump)
+
+    spawnObstacle()
+
+    function spawnObstacle() {
+        obstCount++
+        if (obstCount % 10 === 0) gameSpeed = +(gameSpeed * 1.05).toFixed(2)
+        let obstX = 105
+        const obst = document.createElement('div')
+        obst.classList.add('obstacle')
+        screenGame.insertAdjacentElement('beforeend', obst)
+        setInterval(() => {
+            obst.style.left = `${obstX -= gameSpeed}%`;
+            if (obstX < -5) obst.remove()
+            isAlive(obstX)
+        }, 15);
+        const spawnTime = getRandomNum(500, 2500)
+        setTimeout(() => {
+            spawnObstacle()
+        }, spawnTime);
+    }
+    function jump() {
+        if (!char.classList.contains('jump')) {
+            char.classList.add('jump')
+            setTimeout(() => {
+                char.classList.remove('jump')
+            }, 500)
+        }
+    }
+    function isAlive(obstX) {
+        const charY = parseInt(window.getComputedStyle(char).bottom)
+        if (charY <= 110 && (obstX <= 25 && obstX >= 20)) {
+            console.log('you died');
+        }
+    }
+}
+function getRandomNum(min, max) {
+    const a = Math.floor(Math.random() * (max - min) + min)
+    console.log(a);
+    return a
 }
 function getGameHTML() {
     return `
-    
+        <div class="game__ground"></div>
+        <div class="game__bg"></div>
+        <div class="game__character"></div>
     `
 }
