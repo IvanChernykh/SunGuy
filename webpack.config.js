5f1f05d97053de36567fd4cc23a6bad6ffaa28aa
Ivan Chernykh <ivanchernykh98@gmail.com>
2021-07-02 22:49:28 +0300

    ddd

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './js/index.js',
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     plugins: [
         new HTMLWebpackPlugin({
